67c1ee67853f72e2bef5cff4d77a2fe296aeaccb
NobHonFor <stanislavsolo@gmail.com>
2021-08-30 05:07:37 +0300

    Добавил поясняющие print при выполнении программы

--- 3-d_hw_part_1.py
+++ 3-d_hw_part_1.py
@@ -1,8 +1,8 @@
 string1 = 'Съешь еще этих мягких французских булок ДА выпей же чаю'
-
-print(string1.split(" ")[3].upper())
-print(string1.split(" ")[6].lower())
-print(string1.split(" ")[7][2])
-
+print('Строка: ', string1)
+print('4-е слово в строке в верхнем регистре: ', string1.split(" ")[3].upper())
+print('7-е слово в строке в нижнем регистре: ', string1.split(" ")[6].lower())
+print('3-я буква в 8-м слове: ', string1.split(" ")[7][2])
+print('Каждое из слов в строке: ')
 for i in range(len(string1.split(" "))):
     print(string1.split(" ")[i])
