82b1636bb6a900dbda5bf8a42069bc076e5054d8
NobHonFor <stanislavsolo@gmail.com>
2021-08-31 20:10:25 +0300

    Оптимизировал скрпит, ввел переменную work_str

--- 3-d_hw_part_1.py
+++ 3-d_hw_part_1.py
@@ -1,8 +1,9 @@
 string1 = 'Съешь еще этих мягких французских булок ДА выпей же чаю'
+work_str = string1.split(" ")
 print('Строка: ', string1)
-print('4-е слово в строке в верхнем регистре: ', string1.split(" ")[3].upper())
-print('7-е слово в строке в нижнем регистре: ', string1.split(" ")[6].lower())
-print('3-я буква в 8-м слове: ', string1.split(" ")[7][2])
+print('4-е слово в строке в верхнем регистре: ', work_str[3].upper())
+print('7-е слово в строке в нижнем регистре: ', work_str[6].lower())
+print('3-я буква в 8-м слове: ', work_str[7][2])
 print('Каждое из слов в строке: ')
-for i in range(len(string1.split(" "))):
-    print(string1.split(" ")[i])
+for i in range(len(work_str)):
+    print(work_str[i])
