602b220ebfbbd3d9e32bcf2c213f6ebef233a9d3
NobHonFor <stanislavsolo@gmail.com>
2021-09-21 05:01:41 +0300

    Выполнил 4-ю часть 5-го задания

---
+++ 5-th_hw_part_4.py
@@ -0,0 +1,11 @@
+while True:
+    with open('hw_5.4.txt', 'a', encoding='utf-8') as file:
+        file.write(input('Введите число:') + '\n')
+        file.write(input('Введите запись:') + '\n')
+        file.close()
+    desc = input('Продолжить работу с журналом да\нет:')
+    if desc == 'нет':
+        break
+with open('hw_5.4.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        print(line)
